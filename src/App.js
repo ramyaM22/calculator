6dc5791202c8161dd0d7394ffd52482101450977
ramyaM22 <ramyamaddukuri22@gmail.com>
2023-08-16 15:14:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+const App=()=>{
+  const[input,setInput]=useState("");
+  const[result,setResult]=useState(0);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const Handler=(e)=>{
+  setInput(e.target.value);
+  }
+  function del(){
+    setInput(input.slice(0,-1));
+
+  }
+    return(
+      <div className='main'>
+    <center>
+        <input type="text" value={input} name="input" onChange={Handler}/>
+        <br/>
+        <button onClick={()=>setResult(eval(input))}>Result</button>
+        <br/>
+        <h2>{result}</h2><br/>
+        {/* button for clear*/}
+        <button onClick={del}>Delete</button>&nbsp;
+        <button onClick={()=>setInput("")}>Clear</button>
+         <br/>
+        
+         
+         <button onClick={()=>setInput(input+"1")}>1</button>
+         <button onClick={()=>setInput(input+"2")}>2</button>
+         <button onClick={()=>setInput(input+"3")}>3</button>
+         <button onClick={()=>setInput(input+"+")}>+</button><br/>
+         <button onClick={()=>setInput(input+"4")}>4</button>
+         <button onClick={()=>setInput(input+"5")}>5</button>
+          <button onClick={()=>setInput(input+"6")}>6</button>
+           <button onClick={()=>setInput(input+"-")}>-</button><br/>
+         <button onClick={()=>setInput(input+"7")}>7</button>
+         <button onClick={()=>setInput(input+"8")}>8</button>
+         <button onClick={()=>setInput(input+"9")}>9</button>
+         <button onClick={()=>setInput(input+"*")}>*</button><br/>
+         <button onClick={()=>setInput(input+"0")}>0</button>
+         <button onClick={()=>setInput(input+"%")}>%</button>
+         <button onClick={()=>setInput(input+"/")}>/</button>
+         <button onClick={()=>setInput(input+".")}>.</button> <br/>   
+    </center>
     </div>
-  );
+  )
 }
-
 export default App;
